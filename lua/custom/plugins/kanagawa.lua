fd3601a71542a0e4b617d30128e7910798411388
Adam Tetz <mail@adamtetz.nl>
2025-02-14 12:27:29 +0100

    change theme.

--- lua/custom/plugins/kanagawa.lua
+++
@@ -1,8 +0,0 @@
--- File: lua/custom/plugins/autopairs.lua
-
-return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    require("kanagawa").setup {}
-  end,
-}
---
+++ lua/custom/plugins/theme.lua
@@ -0,0 +1,23 @@
+-- File: lua/custom/plugins/autopairs.lua
+
+--[[return {
+  "rebelot/kanagawa.nvim",
+  config = function()
+    require("kanagawa").setup {}
+  end,
+}
+--]]
+
+return {
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup {
+      -- ...
+    }
+
+    vim.cmd 'colorscheme github_light'
+  end,
+}
